4654b9d9c18240aa24bc58a8e241061412008cc0
Skackovm <Skackovm@DUB-MSW7.reynolds.ad>
2012-03-19 21:44:25 +0000

    added jsTree demo

---
+++ jDirectMapWeb/war/jquery_jdirectmap/jquery.jdirectmap.jstree.js
@@ -0,0 +1,173 @@
+	
+	function helper_read_xml(){
+
+			var jTreeSource = new jDirectMapTreeProcessor("TEXT",$("#source_xml_area").val(), $("#tree_source"));
+			var jTreeDestination = new jDirectMapTreeProcessor("TEXT",$("#dest_xml_area").val(), $("#tree_destination"));
+			
+			
+	}	
+	
+	function helper_grid(){
+	
+	$("#tree_destination").bind("move_node.jstree", function (e, data) 
+				{ 
+							
+				if(data.rslt.cy == true)
+				{
+					$.jstree.rollback(data.rlbk);
+					
+						// index calculation  TODO : don't add already existing mapping possible through .unique() function 187 
+						// Given an array of DOM elements, returns an array of the unique elements in the original array.
+						var numberOfRecords = jQuery("#mapping_list").getGridParam("records");
+						
+						jQuery("#mapping_list").jqGrid('addRowData',++numberOfRecords,{id: numberOfRecords, sparam: data.rslt.o.attr("id"),dparam: data.rslt.r.attr("id") } );
+					
+                }   	
+				});
+				
+	$("#tree_source").bind("move_node.jstree", function (e, data) 
+				{ 
+							
+				if(data.rslt.cy == true)
+				{
+					$.jstree.rollback(data.rlbk);
+					
+						// index calculation  TODO : don't add already existing mapping possible through .unique() function 187 
+						// Given an array of DOM elements, returns an array of the unique elements in the original array.
+						var numberOfRecords = jQuery("#mapping_list").getGridParam("records");
+						
+						jQuery("#mapping_list").jqGrid('addRowData',++numberOfRecords,{id: numberOfRecords, sparam: data.rslt.o.attr("id"),dparam: data.rslt.r.attr("id") } );
+					
+                }   	
+				});		   
+		
+	var $table = $("#mapping_list");	
+	$table.jqGrid({        
+				datatype: "local",
+				colNames:['id','Source Parameter', 'Destination Parameter'],
+				colModel:[
+					{name:'id',index:'id', width:1 , sortable: false},
+					{name:'sparam',index:'sparam', width:280 , sortable: false},
+					{name:'dparam',index:'dparam', width:280, align:"right",  sortable: false}  		
+				],
+				sortname: 'id',
+				viewrecords: false,
+				height: "100%",
+				width:"auto",
+				editurl:"someurl.php", // local processing
+				pager: '#gridpager'  // remove the data
+	});
+
+	
+	
+	
+	
+	$("#deletedata").click(function(){
+				var tableData = new Array();
+				var ids = '';
+				$table.delRowData($table.getGridParam('selrow'));
+				ids = $table.getDataIDs();
+					for(var i = 0; i < ids.length; i++){
+						 tableData[i] = $table.getRowData(ids[i]);
+						 tableData[i].id = i + 1;
+					 }
+				$table.clearGridData(false);
+					for(i = 0; i < tableData.length; i++){
+						 $table.addRowData(i + 1, tableData[i]);
+					}
+				});
+
+	$("#getrow").click(function(){
+				 var dataString = '';
+				 var ids = $table.getDataIDs();
+				 dataString += 'Selected Row: ' + $table.getGridParam('selrow') + '\nRow ID: ' + ids[$table.getGridParam('selrow') - 1];
+				 alert(dataString);
+				});	
+	
+	$("#getdata").click(function(){
+				 var ids = $table.getDataIDs();
+				 var dataString = '';
+					 for(var i = 0; i < ids.length; i++){
+						dataString += 'postion: ' + i + ' ' + ids[i] + '\n';
+					 }
+				 alert(dataString);
+				 });
+	
+	$("#clear").click(function(){
+					// get IDs of all the rows odf jqGrid 
+					var rowIds = $table.jqGrid('getDataIDs');
+					// iterate through the rows and delete each of them
+					for(var i=0,len=rowIds.length;i<len;i++){
+						var currRow = rowIds[i];
+						$table.jqGrid('delRowData', currRow);
+					}	
+					
+				 });
+				 
+	$("#import").click(function(){
+					// get IDs of all the rows odf jqGrid 
+					var rowIds = $table.jqGrid('getDataIDs');
+					// iterate through the rows and delete each of them
+					for(var i=0,len=rowIds.length;i<len;i++){
+						var currRow = rowIds[i];
+						$table.jqGrid('delRowData', currRow);
+					}	
+					$("form").show();
+					//$("#mapping_main").hide();
+				 });
+				 			 
+	$("#export").click(function(){
+					alert("Check out https://github.com/dcneiner/Downloadify ");
+					
+					helper_grid();
+					
+					$.download('customer.json','filename=mySpreadsheet&format=json&content=' + $table );
+
+
+				 });				
+				 
+	$("#moveup").click(function(){
+				   move('up');
+				 });
+				
+	$("#movedown").click(function(){
+				   move('down');
+				 });
+
+				 function move(direction){
+		 if($table.getGridParam('selrow')){
+		 var ids = $table.getDataIDs();
+		 var temp = 0;
+		 var currRow = ids[ $table.getGridParam('selrow') - 1 ];
+		 if(direction === 'up' && currRow > 1){
+		 var r1 = $table.getRowData(currRow-1);
+		 var r2 = $table.getRowData(currRow);
+		 $table.delRowData(currRow-1);
+		 $table.delRowData(currRow);
+		 temp = r1.id;
+		 r1.id = r2.id;
+		 r2.id = temp;
+		 $table.addRowData(r2.id, r2);
+		 $table.addRowData(r1.id, r1);
+		 }
+		 if(direction === 'down' && currRow < (ids.length)){
+		 var r1 = $table.getRowData(currRow);
+		 var r2 = $table.getRowData(parseInt(currRow)+1);
+		 $table.delRowData(currRow);
+		 $table.delRowData(parseInt(currRow)+1);
+		 temp = r1.id;
+		 r1.id = r2.id;
+		 r2.id = temp;
+		 $table.addRowData(r1.id, r1);
+		 $table.addRowData(r2.id, r2);
+		 }
+		 // Sort the table   
+		 $table.setGridParam({sortname:'id'}).trigger('reloadGrid');
+		 }
+	}
+	
+	}
+	
+
+	
+	
---
+++ jDirectMapWeb/war/jquery_jdirectmap/jquery.jdirectmap.treeprocessor.jstree.js
@@ -0,0 +1,190 @@
+/*
+ * jDirectMapTreeProcessor Class
+ * version: 1.0 (03-18-2012)
+ * 
+ * Copyright (c) 2012 Michal Skackov (directmapping.appspot.com/jdirectmap)
+ * 
+ * @requires jQuery v1.7.1 or later
+ * @requires jsTree 1.0-rc1 or later
+ *
+ * @extending UIMTreeProcessor version: 1.0 (11-16-2010)  Copyright (c) 2010 Vlad Shamgin (uimonster.com)
+ *
+ * @todo Supports JSON to JSON transformation 
+ *	 jsTree 
+ *	 zTree 
+ *	 Dynatree
+ * @todo Supports XML to JSON transformation  
+ *	jsTree
+ *	zTree
+ *	Dynatree
+ * @todo Supports XSD to JSON transformation
+ *	jsTree
+ *	zTree
+ *	Dynatree
+ *
+ * Examples and documentation at: directmapping.appspot.com/jdirectmap
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
+
+
+ function jDirectMapTreeProcessor(data, tree_element) {
+	this.tree_element = null;
+	this.data = data;
+	this.tree_element = tree_element;
+}
+
+/*
+* jDirectMapTreeProcessor
+*
+*/
+function jDirectMapTreeProcessor(input_type, input, tree_element) {
+	this.tree_element = null;
+	if(input_type == "AJAX"){
+		$.ajax({
+				type: "GET",
+				url: input,
+				dataType:"xml",
+				cache: false,
+				beforeSend:function(){
+					//do something before send 
+					alert("URL: " + input);
+				},
+				success: function(data){
+					this.data = data
+				},
+				error:function(e){
+					alert("Error: "+e);
+				}
+			});
+	}else if(input_type == "TEXT"){
+		this.data = $.parseXML(input);
+	}else{
+		this.data = data;
+	}
+	this.tree_element = tree_element;
+	
+	
+	//Find root:
+	var _root = $(this.data).children(':first-child');
+	var _a_feed = new Array();
+
+	this.vsTraverse($(_root), _a_feed, "" );
+	var _treedata = [{"data":_root[0].nodeName,"attr" : { "id" :  "/" + _root[0].nodeName },"children":_a_feed, "state":"open"}];
+	this.initTree(_treedata);
+	
+
+}
+
+
+jDirectMapTreeProcessor.prototype.initTree = function(data){
+	this.tree_element.jstree({
+		"json_data" : {
+			"data":data,
+			"progressive_render":"true"
+		},
+		"rules" : { drag_copy:"on", multitree:true },
+		"dnd" : { 
+		 "after": false,
+		 "before": false,
+		 "inside": true,
+		 "drop_target" : false,
+		 "drag_target" : false
+		 },
+		 "crrm" : { 
+				"after": false,
+				"before": false,
+				"inside": true,
+            "move" : {
+				"always_copy" : "multitree",
+            }
+        },
+		"plugins" : [ "themes", "json_data", "ui" ,"dnd","crrm" ]
+	});
+}
+
+
+
+/** recursive processing of xml elements bottom down 
+ *
+ * @input1 node to process
+ * @input2 json object to push data into
+ * @input3 parent xpath 
+ *
+ **/
+jDirectMapTreeProcessor.prototype.vsTraverse = function(node, arr , parent){
+	//get children elements
+	var _ch = $(node).children();
+	
+	//for each element - childerns of @input1 node 
+	for(var i=0; i<_ch.length; i++){
+		var _vsArr = new Array();
+		//recursive travel all children
+
+			 /**
+			  * @input1 current child _ch[i] to process
+			  * @input2 new _vsArr json object to push data into
+			  * @input3 parent xpath parrent plus current node
+			**/
+			this.vsTraverse(_ch[i], _vsArr , parent + "/" +  _ch[i].parentNode.nodeName );
+		
+			//get attributes values 
+			var _a_att = this.vsTraverseAtt(_ch[i] , parent);
+			
+		// if there element HAS attributes add attributes as json data 
+		// format [{data: Attributes [element] , attr : {id : /xpath/element}, 	children: [JSON _a_att]}]
+		if(null!=_a_att){
+		//console.log("attributes");
+		//console.log("format [{data: Attributes [element] , attr : {id : /xpath/element}, 	children: [JSON _a_att]}]");
+		//console.log([{"data":"Attributes "+"["+_ch[i].nodeName+"]", "attr" : { "id" : parent + "/" + _ch[i].parentNode.nodeName + "/" +_ch[i].nodeName }, "children":_a_att}]);
+					_vsArr.push([{"data":"Attributes "+"["+_ch[i].nodeName+"]", "attr" : { "id" : parent + "/" + _ch[i].parentNode.nodeName + "/" +_ch[i].nodeName }, "children":_a_att}]);
+		}
+		//if element has children and frist child is XML DOM 3	TEXT_NODE
+		// format [{data: Attributes [element] , attr : {id : /xpath/element}, 	children: [JSON _vsArr], state:close}]
+		// + ": " + _ch[i].firstChild.textContent
+		if(null!=_ch[i].firstChild && 3 ==_ch[i].firstChild.nodeType){
+		//console.log(_ch[i].nodeName);
+		//console.log("format [{data: Attributes [element] , attr : {id : /xpath/element}, 	children: [JSON _vsArr], state:close}]");
+		//console.log([{"data":_ch[i].nodeName , "attr" : { "id" : parent + "/" + _ch[i].parentNode.nodeName + "/" +_ch[i].nodeName }, "children":_vsArr, "state":"close"}]);
+			if(0 == _vsArr.length){
+				arr.push([{"data":_ch[i].nodeName , "attr" : { "id" : parent + "/" + _ch[i].parentNode.nodeName + "/" +_ch[i].nodeName }}]);
+		
+			}else{
+				arr.push([{"data":_ch[i].nodeName , "attr" : { "id" : parent + "/" + _ch[i].parentNode.nodeName + "/" +_ch[i].nodeName }, "children":_vsArr, "state":"close"}]);
+			}
+		}
+		// else there are no children ie element is leaf 
+		// format [{data: Attributes [element] , attr : {id : /xpath/element}, state:close}]
+		else{
+			 arr.push([{"data":_ch[i].nodeName, "attr" : { "id" : parent + "/" + _ch[i].parentNode.nodeName + "/" +_ch[i].nodeName }}]);
+		}
+	}	
+}
+
+/** processing  attributes of xml element
+ *
+ * @input1 node to process
+ * @input2 parent xpath 
+ * @output array of attributes in json format
+ *
+ **/
+jDirectMapTreeProcessor.prototype.vsTraverseAtt = function(node, parent){
+	var _a_atts = null;
+	//only when attributes exists else return null
+	if(null!=node.attributes && node.attributes.length > 0){
+		_a_atts = new Array();
+		
+		//for each attribute of element
+		for(var i=0; i<node.attributes.length; i++){
+		//	console.log("leaf");
+		//	console.log("format [{data: ATTRIBUTE_NAME , attr : {id : /xpath/element.ATTRIBUTE_NAME}}]");
+		//	console.log([{ "data": node.attributes[i].nodeName ,  "attr" : { "id" : parent + "/" + node.nodeName +"."+ node.attributes[i].nodeName }}]  );
+			//_a_atts.push([node.attributes[i].nodeName + ":" + node.attributes[i].nodeValue , "attr" : { "id" : _ch[i].parentNode.nodeName + "/" +_ch[i].nodeName }, "attr" : { "idn" : i}]);
+			// format [{data: ATTRIBUTE_NAME , attr : {id : /xpath/element.ATTRIBUTE_NAME}}]
+			_a_atts.push([{ "data": node.attributes[i].nodeName ,  "attr" : { "id" : parent + "/" + node.nodeName +"."+ node.attributes[i].nodeName }}]  );
+		}
+	}
+	return _a_atts;
+}
+
